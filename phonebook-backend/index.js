13ad27713fa43d46e9470da8e1166316d4ee8fb4
Joel Moran <joelbmoran@gmail.com>
2022-04-29 11:00:12 +0700

    Make phonebook frontend and backend work together (exercise 3.9)

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
-
 const app = express()
 
 morgan.token('bodyContent', function (req, res) { return JSON.stringify(req.body) })
@@ -10,7 +9,7 @@ morgan.token('bodyContent', function (req, res) { return JSON.stringify(req.body
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
-app.use(morgan(':bodyContent'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :bodyContent'))
 
 
 let persons = [
