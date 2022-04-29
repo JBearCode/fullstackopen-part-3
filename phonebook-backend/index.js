ea938c534e64bf1cdc68e97b6b92dfb5a510f25c
Joel Moran <joelbmoran@gmail.com>
2022-04-29 11:43:11 +0700

    Prepare full app for Heroku deployment

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -6,6 +6,7 @@ const app = express()
 
 morgan.token('bodyContent', function (req, res) { return JSON.stringify(req.body) })
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
