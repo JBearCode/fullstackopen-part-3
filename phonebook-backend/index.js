06655e5bc41f262474dad73a20dbb288864d41c7
Joel Moran <joelbmoran@gmail.com>
2022-05-02 21:52:47 +0700

    Remove hardcoded contacts

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -14,30 +14,6 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :bodyContent'))
 
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
   app.get('/', (request, response) => {
     response.send('<h1>Hello there!</h1>')
   })
